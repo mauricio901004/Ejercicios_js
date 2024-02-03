6f4d3cf285072e165b525198a17eb746a1f5b9c1
mauricio901004 <maoamb@gmail.com>
2024-02-03 20:10:03 +0100

    Ejercicio 1

---
+++ Ercicio 1/index.js
@@ -0,0 +1,13 @@
+//Determinar el area de un cubo 
+//Base 16cm*23cm
+//Altura 13cm
+
+const volunen = 16*23*13
+console.log ("volumen",volunen + "cm3");
+
+//Determinar el area
+
+const areaLateral = 23*13;
+const areaBase = 16*23;
+const areaTotal = areaLateral + 2*areaBase;
+console.log ("Area Total", areaTotal + "cm2")
