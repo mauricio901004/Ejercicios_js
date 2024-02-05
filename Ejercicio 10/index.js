e70a7cd8e3eaf08f16b508a947d2eaf8adff2f8f
mauricio901004 <maoamb@gmail.com>
2024-02-05 20:01:27 +0100

    ejercicio 10

---
+++ Ejercicio 10/index.js
@@ -0,0 +1,9 @@
+function checkNumber(numero) {
+
+    if (numero %2==0) {
+        return  "el numero es par"
+    } else {
+        return "el numero es impar"
+    }
+    
+}
---
+++ Ejercicio 9/index.js
@@ -0,0 +1,9 @@
+let celsius = 30
+
+function deCelsiusAFahrenheit(celsius) {
+    let fahrenheit = 9/5*celsius+32
+    return fahrenheit
+        
+}
+
+console.log(celsius+ " grados celsius son "+ deCelsiusAFahrenheit(celsius)+ " fahrenheit.")
