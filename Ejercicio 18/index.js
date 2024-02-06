3f6b22b5883e1ef405109ba3dc54872dc06c3765
mauricio901004 <maoamb@gmail.com>
2024-02-06 23:15:28 +0100

    ejercicio 18

---
+++ Ejercicio 18/index.js
@@ -0,0 +1,9 @@
+document.getElementById("btn").addEventListener("click", botonAlerta);
+function botonAlerta() {
+    onclick= alert('Guardado')
+}
+
+const element = document.getElementsByTagName("lable");
+const element1 = element[0]
+
+
